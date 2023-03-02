69d844281db4a06fd630f1ce0c56630a1d9b852c
Amit Yadav <amit.yadav@go-mmt.com>
2023-03-02 13:23:02 +0530

    Listing all fields in revise model

--- revision/admin.py
+++ revision/admin.py
@@ -6,7 +6,7 @@ from .models import Revise
 
 
 class ReviseAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in Revise._meta.get_fields()]
 
 
 admin.site.register(Revise, ReviseAdmin)
