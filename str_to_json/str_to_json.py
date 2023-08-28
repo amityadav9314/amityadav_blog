01c085abbc4e08b764d22033c44f6ba8b153a32b
amit.yadav@go-mmt.com <amit.yadav@go-mmt.com>
2023-08-28 19:33:03 +0530

    Added one util to convert str to json

---
+++ str_to_json/str_to_json.py
@@ -0,0 +1,35 @@
+import json
+import re
+import os
+
+
+def get_current_path():
+    current_working_directory = os.getcwd()
+    return current_working_directory
+
+
+read_file_path = get_current_path() + "/str_file.txt"
+write_file_path = get_current_path() + "/json_file.json"
+
+
+def convert_json(str_file):
+    json_object = str_file.read()
+    json_object = json.loads(json_object)
+    if isinstance(json_object, dict):
+        json_object = json.dumps(json_object, indent=4)
+
+    json_object = re.sub(r'\\n', "", json_object)
+    json_object = re.sub(r'\\', "", json_object)
+    json_object = re.sub(r'\\n', "", json_object)
+    json_object = re.sub(r'"{', "{", json_object)
+    json_object = re.sub(r'}"', "}", json_object)
+    # Create a new file.
+    with open(write_file_path, 'w') as new_json_file:
+        new_json_file.write(json_object)
+    return json_object
+
+
+if __name__ == '__main__':
+    with open(read_file_path, 'r') as json_file:
+        json_object = convert_json(json_file)
+        print(json_object)
